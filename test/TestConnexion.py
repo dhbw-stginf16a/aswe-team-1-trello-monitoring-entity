b8043b697ed06b4001c8597a4a3797f7ce50b410
Thore <thore@kruess.xyz>
2019-04-01 01:12:37 +0200

    remove old test relict

--- test/TestConnexion.py
+++ test/TestConnexion.py
@@ -14,7 +14,6 @@ class TestConnexion:
     cache: dict = {}
     CENTRAL_NODE_BASE_URL = os.environ.setdefault('CENTRAL_NODE_BASE_URL', 'http://localhost:8080/api/v1')
 
-    #@requests_mock.mock()
     @pytest.fixture(scope='function')
     def client(self, requests_mock):
         requests_mock.post(f'{self.CENTRAL_NODE_BASE_URL}/monitoring', text='', status_code=204)
