ddfb05778e5d9b73142f441332c3b6ccda094b59
Thore <thore@kruess.xyz>
2019-04-03 22:25:46 +0200

    Push test coverage to 100%

---
+++ test/test_registration.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import time
+
+from api.models.RegistrationThread import RegistrationThread
+
+class TestRegistration:
+    centralNode = 'http://localhost:8080/api/v1'
+    url = 'http://localhost:5000/api/v1'
+    concern = 'test'
+
+
+    def test_retry(self):
+        reg = RegistrationThread(self.centralNode, self.url, self.concern)
+
+        reg.start()
+        # Wait for the thread to fail at least once
+        time.sleep(3)
+        reg.stop()
+
+    def test_success(self, requests_mock):
+        requests_mock.post(f'{self.centralNode}/monitoring', text='', status_code=204)
+        reg = RegistrationThread(self.centralNode, self.url, self.concern)
+
+        reg.start()
+        # Wait for the thread to fail at least once
+        reg.stop()
